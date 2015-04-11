33823f3e92bb4c7a320e129a6e869906b6befc3b
inexistence <janbean@163.com>
2015-04-11 15:38:14 +0800

    view page in browser

--- src/com/scut/picturelibrary/adapter/InSimiGridViewAdapter.java
+++ src/com/scut/picturelibrary/adapter/InSimiGridViewAdapter.java
@@ -1,6 +1,7 @@
 package com.scut.picturelibrary.adapter;
 
 import java.util.List;
+import java.util.Map;
 
 import android.content.Context;
 import android.view.View;
@@ -16,14 +17,25 @@ import com.nostra13.universalimageloader.core.ImageLoader;
  * @author 黄建斌
  * 
  */
-public class SimiAdapter extends ArrayAdapter<String> {
-	private List<String> mSimiImagesUrlList;
+public class InSimiGridViewAdapter extends ArrayAdapter<Map<String, String>> {
+	private List<Map<String, String>> mSimiImagesUrlList;
 	private int mImageViewResourceId;
 	Context mContext;
 	int mResource;
 
-	public SimiAdapter(Context context, int resource, int imageViewResourceId,
-			List<String> objects) {
+	public static final String IMAGE_URL = "imageUrl";
+	public static final String FROM_URL = "fromUrl";
+
+	/**
+	 * 
+	 * @param context
+	 * @param resource
+	 * @param imageViewResourceId
+	 * @param objects
+	 *            List<String[]> String[0]:image url,String[1]:fromPageURL
+	 */
+	public InSimiGridViewAdapter(Context context, int resource,
+			int imageViewResourceId, List<Map<String, String>> objects) {
 		super(context, resource, imageViewResourceId, objects);
 		mContext = context;
 		mImageViewResourceId = imageViewResourceId;
@@ -46,8 +58,12 @@ public class SimiAdapter extends ArrayAdapter<String> {
 			convertView.setTag(imageView);
 		}
 		imageView = (ImageView) convertView.getTag();
-		String url = getItem(position);
+		String url = getItem(position).get(IMAGE_URL);
 		ImageLoader.getInstance().displayImage(url, imageView);
 		return convertView;
+	}
+
+	public String getFromURL(int position) {
+		return getItem(position).get(FROM_URL);
 	}
 }
